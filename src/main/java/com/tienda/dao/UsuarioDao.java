85c1fe8a7aad347bd11033a4fd93014f0d0b530d
natha <natha@DESKTOP-L755V5E>
2025-07-22 20:19:04 -0600

    Semana11

--- src/main/java/com/tienda/dao/UsuarioDao.java
+++ src/main/java/com/tienda/dao/UsuarioDao.java
@@ -9,5 +9,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
     Usuario findByUsername(String username);
+    
+
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
 
